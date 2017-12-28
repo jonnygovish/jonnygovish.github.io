fae42f5abb21e5113a3fc85a5ab629aea905c318
John <jonnygovish@gmail.com>
2017-12-28 10:49:14 +0300

    Modifications

--- js/scripts.js
+++ js/scripts.js
@@ -186,34 +186,6 @@ jQuery(document).ready(function ($) {
     });
 
 
-    // Google Map
-
-
-    // main directions
-    map = new GMaps({
-        position: "TOP_CENTER",
-        el: '#map',
-        lat: 23.790223,
-        lng: 90.414036,
-        zoom: 13,
-        zoomControl: true,
-        zoomControlOpt: {
-            style: 'SMALL',
-            position: 'TOP_LEFT'
-        },
-        panControl: false,
-        scrollwheel: false
-    });
-    // add address markers
-    map.addMarker({
-        lat: 23.790223,
-        lng: 90.414036,
-        title: 'Codetic',
-        infoWindow: {
-            content: '<p> Mirpur , Dhaka</p>'
-        }
-    });
-
 
     // Contact Form
 
@@ -261,7 +233,6 @@ jQuery(document).ready(function ($) {
 
 /*
  * BG Loaded
- * Copyright (c) 2014 Jonathan Catmull
  * Licensed under the MIT license.
  */
 (function ($) {
