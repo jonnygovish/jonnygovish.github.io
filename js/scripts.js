c2f3fe059c34bab36e7f9f80c762bb552be3b406
John <jonnygovish@gmail.com>
2017-12-29 18:19:46 +0300

    Minor changes

--- js/scripts.js
+++ js/scripts.js
@@ -189,44 +189,44 @@ jQuery(document).ready(function ($) {
 
     // Contact Form
 
-    $('form#contactForm button.submit').click(function () {
+    // $('form#contactForm button.submit').click(function () {
 
-        $('#image-loader').fadeIn();
+    //     $('#image-loader').fadeIn();
 
-        var contactName = $('#contactForm #contactName').val();
-        var contactEmail = $('#contactForm #contactEmail').val();
-        var contactMessage = $('#contactForm #contactMessage').val();
+    //     var contactName = $('#contactForm #contactName').val();
+    //     var contactEmail = $('#contactForm #contactEmail').val();
+    //     var contactMessage = $('#contactForm #contactMessage').val();
 
-        var data = 'contactName=' + contactName + '&contactEmail=' + contactEmail + '&contactMessage=' + contactMessage;
+    //     var data = 'contactName=' + contactName + '&contactEmail=' + contactEmail + '&contactMessage=' + contactMessage;
 
-        $.ajax({
+    //     $.ajax({
 
-            type: "POST",
-            url: "inc/sendEmail.php",
-            data: data,
-            success: function (msg) {
+    //         type: "POST",
+    //         url: "inc/sendEmail.php",
+    //         data: data,
+    //         success: function (msg) {
 
-                // Message was sent
-                if (msg == 'OK') {
-                    $('#image-loader').fadeOut();
-                    $('#message-warning').hide();
-                    $('#contactForm').fadeOut();
-                    $('#message-success').fadeIn();
-                }
-                // There was an error
-                else {
-                    $('#image-loader').fadeOut();
-                    $('#message-warning').html(msg);
-                    $('#message-warning').fadeIn();
-                }
+    //             // Message was sent
+    //             if (msg == 'OK') {
+    //                 $('#image-loader').fadeOut();
+    //                 $('#message-warning').hide();
+    //                 $('#contactForm').fadeOut();
+    //                 $('#message-success').fadeIn();
+    //             }
+    //             // There was an error
+    //             else {
+    //                 $('#image-loader').fadeOut();
+    //                 $('#message-warning').html(msg);
+    //                 $('#message-warning').fadeIn();
+    //             }
 
-            }
+    //         }
 
-        });
-        return false;
-    });
+    //     });
+    //     return false;
+    // });
 
-    // Contact form end
+    // // Contact form end
 
 
 });
